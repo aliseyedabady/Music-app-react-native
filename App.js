bc3fa5d929dcced4b3ba6df24b4726f906b9b03b
Ali Seyed Abady <ialiseyed@gmail.com>
2021-11-22 10:59:27 +0330

    fawf

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
+
         <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
